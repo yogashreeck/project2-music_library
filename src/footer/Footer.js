e19beac9dd28c75bf84abf86be3ecef372c127da
Yogashreeck <ykumarostb1@gmail.com>
2019-08-27 12:52:32 +0530

    media query

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -11,13 +11,13 @@ class Footer extends Component {
             <div className="row">
               <div className="col-md-9">
                 <div className="row">
-                  <div className="col-md-5">
+                  <div className="col-md-5 about">
                     <h5 className="footer-heading mb-4">About Us</h5>
                     <p>We bring together your favourite music services and join up listening, watching and sharing to
                       connect your musical world.Below you can visualise, in real-time, the listening habits and trends
                       of My Music global community.</p>
                   </div>
-                  <div className="col-md-3 ml-auto">
+                  <div className="col-md-3 ml-auto small">
                     <h5 className="footer-heading mb-4">Quick Links</h5>
                     <ul className="list-unstyled">
                       <li><a >About Us</a></li>
@@ -26,18 +26,17 @@ class Footer extends Component {
                       <li><a>Contact Us</a></li>
                     </ul>
                   </div>
-                  <div class="col-md-3">
-                    <h5 class="footer-heading mb-4">Follow Us</h5>
+                  <div className="col-md-3 small">
+                    <h5 className="footer-heading mb-4">Follow Us</h5>
                     <SocialIcon url="https://twitter.com/" className="foot-icon" />
                     <SocialIcon url="https://www.facebook.com/" className="foot-icon" />
                     <SocialIcon url="https://www.whatsapp.com/" className="foot-icon" />
-                    <SocialIcon url="https://www.email.com/" className="foot-icon" />
                     <SocialIcon url="http://linkedin.com/" className="foot-icon" />
                   </div>
                 </div>
               </div>
-              <div class="col-md-3">
-                <h5 class="footer-heading mb-4">Subscribe</h5>
+              <div className="col-md-3">
+                <h5 className="footer-heading mb-4">Subscribe</h5>
                 <form action="#" method="post" className="footer-subscribe">
                   <div className="input-group mb-3">
                     <input type="text" className="form-control border-secondary text-white bg-transparent" placeholder="Enter Email"
@@ -49,7 +48,7 @@ class Footer extends Component {
                 </form>
               </div>
             </div>
-            <div className="row pt-5 mt-5 text-center">
+            <div className="row pt-5 mt-5 text-center about">
               <div className="col-md-12">
                 <div className="border-top pt-5">
                   <p>
